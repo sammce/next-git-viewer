bc244ff0a433f9959f8f8a40b4515481cf023d04
Sam McElligott <sam.mcelligott3@mail.dcu.ie>
2025-02-27 23:45:44 +0000

    ci: WIP-2 fix api imports

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
 export { default as GitCommitSelector } from "./components/GitCommitSelector";
 
 // Endpoint exports
-export { default as checkout } from "./api/checkout/route";
-export { default as commits } from "./api/commits/route";
+export { default as checkout } from "./api/checkout";
+export { default as commits } from "./api/commits";
 
 // Type exports
 export type { Commit } from "./types/commit";
